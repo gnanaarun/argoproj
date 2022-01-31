479d3463a05cb0941139ead86178cf945f0ff452
gnanaarun <70479850+gnanaarun@users.noreply.github.com>
2022-01-31 20:23:58 +0530

    Update deployment.tf

--- testcode/deployment.tf
+++ testcode/deployment.tf
@@ -1,13 +1,9 @@
 ##Configuring Terraform Backend
-terraform {   
- backend "azurerm" {
- resource_group_name   = bcai-test-poc
- storage_account_name  = bcaipocstg
- container_name        = tfback
- key                   = rs.tf
- }
-}
-resource "azurerm_resource_group" "example" {
-  name     = "var.resource_group_name"
-  location = "West Europe"
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
 }
