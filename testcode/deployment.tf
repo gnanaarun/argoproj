23f3c80465c9591708e3f3fd3905e6f2f7508e86
gnanaarun <70479850+gnanaarun@users.noreply.github.com>
2022-02-14 11:33:12 +0530

    Update deployment.tf

--- testcode/deployment.tf
+++ testcode/deployment.tf
@@ -1,8 +1,9 @@
 ##Configuring Terraform Backend
 
 provider "azurerm" {
-features {} /* subscription_id = "64238fe1-943b-49f8-bff3-9a61736c81af"
+features {}
+subscription_id = "64238fe1-943b-49f8-bff3-9a61736c81af"
 client_id = "ac1f500e-95e2-42b7-a8b4-3a7c484816f2"
 client_secret = "7857ec04-a137-40ba-9871-fbf8b578dba8"
-tenant_id = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4" */
+tenant_id = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4" 
 }
