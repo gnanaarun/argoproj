12254ee48020d3c2e8f1db2ab2c46cb499d188c5
gnanaarun <70479850+gnanaarun@users.noreply.github.com>
2022-02-14 20:29:44 +0530

    Update deployment.tf

--- testcode/deployment.tf
+++ testcode/deployment.tf
@@ -7,3 +7,12 @@ client_id = "ac1f500e-95e2-42b7-a8b4-3a7c484816f2"
 client_secret = "7857ec04-a137-40ba-9871-fbf8b578dba8"
 tenant_id = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4" 
 }
+
+terraform {
+backend "azurerm" {
+resource_group_name = bcai-tf-rg
+storage_account_name = bcaitfstg
+container_name = bcaictnr
+key = storage.tfstate
+}
+}
